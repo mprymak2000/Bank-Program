0ce6865fb3848de69c00ff7202cf8a5696de72b1
mprymak2000 <mprymak2000@gmail.com>
2025-05-27 00:52:19 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -8,7 +8,7 @@ public abstract class Account {
     public abstract boolean withdraw(Currency money);
 
     public void deposit(Currency moneyIn) {
-        if (moneyIn > 0) {
+        if (moneyIn.getValue() > 0) {
             balance.add(moneyIn);
         }
     }
